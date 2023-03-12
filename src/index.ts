5d779a430ba3b9ca25c9ff9128020ecdc4298a57
hocsinhgioitoan <89916615+hocsinhgioitoan@users.noreply.github.com>
2023-03-12 11:30:01 +0700

    refactor(Crafting): change display item

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,20 @@
 
 // Env
 import { config } from "dotenv";
+import express from "express";
 config();
 if (!process.env.mode) process.env.mode = "local";
 // Build Bot
-import('./Class/Client').then((file) => new file.default().build(process.env[`${process.env.mode}_token`]!));
+import("./Class/Client").then((file) =>
+    new file.default().build(process.env[`${process.env.mode}_token`]!)
+);
+
+if (process.env.mode === "host") {
+    const app = express();
+    app.get("/", (req, res) => {
+        res.send("Hello World!");
+    });
+    app.listen(3000, () => {
+        console.log("Example app listening on port 3000!");
+    });
+}
